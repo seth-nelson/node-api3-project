a552b4b56930d410fe6e8e62a85ac1956bb8c5b2
Seth Nelson <seth.nelson@outlook.com>
2020-05-14 18:14:58 -0700

    edited server file to integrate the logger custom middleware

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const postRouter = require('./posts/postRouter');
 
 const server = express();
+
+server.use(express.json());
+server.use(logger);
+server.use('/api/posts, postRouter')
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method);
+  next();
+}
 
 module.exports = server;
